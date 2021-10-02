aac1f219d26ef28b293950b7ae36d44ece470314
wody27 <maxwodyd98@gmail.com>
2021-10-02 15:41:51 +0900

    :tada: Init project

---
+++ Seehyang/Seehyang/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seehyang
+//
+//  Created by Wody on 2021/10/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
