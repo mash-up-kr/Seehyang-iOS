13b895ce4df3456dfb8d12a1bdb9177597b5e18a
Jaeyong <maxwodyd98@gmail.com>
2021-10-17 04:14:58 +0900

    🍎 홈 스토리보드 구성 (#2)

---
+++ Seehyang-iOS/Seehyang-iOS/Sources/Home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  Seehyang-iOS
+//
+//  Created by Wody on 2021/10/17.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
