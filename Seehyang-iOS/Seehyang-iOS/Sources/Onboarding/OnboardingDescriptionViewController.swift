b15402b619fe7395cfab37a7cc2ae08d1f62987b
JeongMinho <jeong.minho@webtoonscorp.com>
2021-11-23 01:34:25 +0900

    :sparkles: onboardingviewcontroller 기본구현

---
+++ Seehyang-iOS/Seehyang-iOS/Sources/Onboarding/OnboardingDescriptionViewController.swift
@@ -0,0 +1,92 @@
+//
+//  OnboardingDescriptionViewController.swift
+//  Seehyang-iOS
+//
+//  Created by JeongMinho on 2021/11/19.
+//
+
+import UIKit
+import SnapKit
+
+class OnboardingDescriptionViewController: UIViewController {
+    
+    // MARK: - Variable
+    lazy var onboardingImageView: UIImageView = {
+        let imageView: UIImageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    let onboardingDescriptionLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    let descriptionLabelView: UIView = {
+        let view: UIView = UIView()
+        view.backgroundColor = .coolgrey6
+        return view
+    }()
+    
+    let firstDescriptionLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.font = .systemFont(ofSize: 18)
+        label.numberOfLines = 2
+        label.text = "다양한 향수 이미지를 \n 통해 눈으로 시향해보세요 :)"
+        label.textAlignment = .center
+        label.textColor = .purple
+        return label
+    }()
+    
+    let secondDescriptionLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.font = .systemFont(ofSize: 12)
+        label.numberOfLines = 2
+        label.text = "직접 사용한 사람들이 올리는 \n 향수에 대한 이미지들을 통해 시향할 수 있습니다."
+        label.textAlignment = .center
+        label.textColor = .grey3
+        return label
+    }()
+    
+    private func setLayout() {
+        self.onboardingImageView.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+            make.leading.trailing.equalToSuperview()
+        }
+        
+        self.descriptionLabelView.snp.makeConstraints { make in
+            make.top.equalTo(self.onboardingImageView.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(183)
+        }
+        
+        self.firstDescriptionLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(9)
+        }
+        
+        self.secondDescriptionLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(firstDescriptionLabel.snp.bottom).offset(24)
+        }
+    }
+    
+    private func setup() {
+        self.view.backgroundColor = .coolgrey6
+        self.view.addSubview(onboardingImageView)
+        self.view.addSubview(descriptionLabelView)
+        self.view.addSubview(onboardingDescriptionLabel)
+        
+        self.descriptionLabelView.addSubview(firstDescriptionLabel)
+        self.descriptionLabelView.addSubview(secondDescriptionLabel)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setup()
+        setLayout()
+    }
+}
+
--- Seehyang-iOS/Seehyang-iOS/Sources/Onboarding/OnboardingViewController.swift
+++ Seehyang-iOS/Seehyang-iOS/Sources/Onboarding/OnboardingViewController.swift
@@ -6,15 +6,59 @@
 //
 
 import UIKit
+import SnapKit
 
 class OnboardingViewController: UIViewController {
     
+    // MARK: - View
+    private var pageViewController: UIPageViewController = {
+        let pageViewController: UIPageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal,
+            options: nil
+        )
+        return pageViewController
+    }()
+    
+    private let pageControl: UIPageControl = {
+        let pageControl: UIPageControl = UIPageControl()
+        pageControl.numberOfPages = 3
+        pageControl.currentPage = 0
+        pageControl.pageIndicatorTintColor = .red
+        pageControl.currentPageIndicatorTintColor = .coolgrey5
+        return pageControl
+    }()
+    
+    private let nextButton: UIButton = {
+        let button: UIButton = UIButton()
+        button.backgroundColor = .purple
+        button.titleLabel?.text = "다음"
+        button.titleLabel?.font = .systemFont(ofSize: 16)
+        button.titleLabel?.textColor = .white
+        return button
+    }()
+
     // MARK: - Variable
-    private let pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .vertical, options: nil)
- 
-    var contentViewController: [UIViewController]? = []
+    var contentViewController: [OnboardingDescriptionViewController]? = []
     
     // MARK: - Method
+    private func setLayout() {
+        view.addSubview(pageControl)
+        view.addSubview(nextButton)
+        
+        nextButton.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().inset(42)
+            make.leading.trailing.equalToSuperview().inset(30)
+            make.height.equalTo(48)
+        }
+        
+        pageControl.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(157)
+            make.bottom.equalTo(nextButton.snp.top).offset(84)
+            make.height.equalTo(12)
+        }
+    }
+
     private func removeTapGestureRecognizer() {
         guard let tapGesture = pageViewController.view.gestureRecognizers?.first(where: { recognizer in
             recognizer is UITapGestureRecognizer
@@ -26,10 +70,16 @@ class OnboardingViewController: UIViewController {
     }
     
     func setPageViewController() {
-        pageViewController.view.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        self.view.addSubview(pageViewController.view)
+        pageViewController.view.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.bottom.equalToSuperview().inset(183)
+        }
+        
         guard let first = contentViewController?.first else {
             return
         }
+        pageViewController.view.backgroundColor = .clear
         pageViewController.setViewControllers([first], direction: .forward, animated: true, completion: nil)
         self.addChild(pageViewController)
         pageViewController.didMove(toParent: self)
@@ -37,9 +87,13 @@ class OnboardingViewController: UIViewController {
     }
     
     func addContentViewController() {
-        contentViewController = [UIColor.red, UIColor.orange, UIColor.yellow].map {
-            let viewController = UIViewController()
-            viewController.view.backgroundColor = $0
+        contentViewController = [
+            UIImage(named: "onboarding1"),
+            UIImage(named: "onboarding2"),
+            UIImage(named: "onboarding3")
+        ].map { image in
+            let viewController = OnboardingDescriptionViewController()
+            viewController.onboardingImageView.image = image
             return viewController
         }
     }
@@ -47,11 +101,14 @@ class OnboardingViewController: UIViewController {
     func setup() {
         pageViewController.dataSource = self
         pageViewController.delegate = self
+        
+        self.view.backgroundColor = .white
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
+        setLayout()
         addContentViewController()
         setPageViewController()
         removeTapGestureRecognizer()
@@ -62,7 +119,8 @@ class OnboardingViewController: UIViewController {
 extension OnboardingViewController: UIPageViewControllerDataSource {
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         
-        guard let index = contentViewController?.firstIndex(of: viewController) else {
+        guard let viewController = viewController as? OnboardingDescriptionViewController,
+              let index = contentViewController?.firstIndex(of: viewController) else {
             return nil
         }
         
@@ -74,7 +132,8 @@ extension OnboardingViewController: UIPageViewControllerDataSource {
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        guard let index = contentViewController?.firstIndex(of: viewController) else {
+        guard let viewController = viewController as? OnboardingDescriptionViewController,
+              let index = contentViewController?.firstIndex(of: viewController) else {
             return nil
         }
         
